9678c6a0f0d9395b7506491332112eebaf363eed
Peter Jin <peterhaijin@gmail.com>
2019-02-05 14:14:02 -0800

    Rename `HeapMem` -> `HeapPod`.

--- src/heap.rs
+++ src/heap.rs
@@ -5,27 +5,27 @@ use std::mem::{align_of, size_of};
 use std::ptr::{NonNull, null_mut, write_bytes};
 use std::slice::{from_raw_parts, from_raw_parts_mut};
 
-pub struct HeapMem<T: Copy> {
+pub struct HeapPod<T: Copy> {
   buf:  *mut T,
   len:  usize,
 }
 
-impl<T> Drop for HeapMem<T> where T: Copy {
+impl<T> Drop for HeapPod<T> where T: Copy {
   fn drop(&mut self) {
     assert!(!self.buf.is_null());
     let p = unsafe { NonNull::new_unchecked(self.buf) };
     self.buf = null_mut();
     match unsafe { Global::default().dealloc_array(p, self.len) } {
-      Err(_) => panic!("failed to dealloc HeapMem"),
+      Err(_) => panic!("failed to dealloc HeapPod"),
       Ok(_) => {}
     }
   }
 }
 
-impl<T> HeapMem<T> where T: Copy {
+impl<T> HeapPod<T> where T: Copy {
   pub unsafe fn alloc(len: usize) -> Self {
     let p = match Global::default().alloc_array(len) {
-      Err(_) => panic!("failed to alloc HeapMem"),
+      Err(_) => panic!("failed to alloc HeapPod"),
       Ok(p) => p,
     };
     let buf: *mut T = p.as_ptr();
@@ -34,7 +34,7 @@ impl<T> HeapMem<T> where T: Copy {
       (0, _) => panic!("malloc returned non-naturally aligned pointer"),
       (_, _) => panic!("size is not a multiple of alignment"),
     }
-    HeapMem{
+    HeapPod{
       buf,
       len,
     }
@@ -46,21 +46,21 @@ impl<T> HeapMem<T> where T: Copy {
   }
 }
 
-impl<T> HeapMem<T> where T: ZeroBits {
+impl<T> HeapPod<T> where T: ZeroBits {
   pub fn zeros(len: usize) -> Self {
-    let mem = unsafe { HeapMem::<T>::alloc(len) };
+    let mem = unsafe { HeapPod::<T>::alloc(len) };
     unsafe { write_bytes::<u8>(mem.as_ptr_mut() as *mut u8, 0, mem.region_len() * size_of::<T>()) };
     mem
   }
 
   pub fn zeros_aligned(len: usize, alignment: usize) -> Self {
-    let mem = unsafe { HeapMem::<T>::alloc_aligned(len, alignment) };
+    let mem = unsafe { HeapPod::<T>::alloc_aligned(len, alignment) };
     unsafe { write_bytes::<u8>(mem.as_ptr_mut() as *mut u8, 0, mem.region_len() * size_of::<T>()) };
     mem
   }
 }
 
-impl<T> PodRegion<T> for HeapMem<T> where T: Copy {
+impl<T> PodRegion<T> for HeapPod<T> where T: Copy {
   fn region_len(&self) -> usize {
     self.len
   }
@@ -78,7 +78,7 @@ impl<T> PodRegion<T> for HeapMem<T> where T: Copy {
   }
 }
 
-impl<T> PodRegionMut<T> for HeapMem<T> where T: Copy {
+impl<T> PodRegionMut<T> for HeapPod<T> where T: Copy {
   fn as_ptr_mut(&self) -> *mut T {
     self.buf
   }
